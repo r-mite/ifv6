420506f653deecb8d4ebca2465a0b584567d02f5
r-mite <universal5b92@gmail.com>
2016-02-25 18:17:40 +0900

    add ec and cli
    first chal

--- dualv6.c
+++ dualv6.c
@@ -20,14 +20,35 @@
 #include <netdb.h>
 
 #define IF_NUM "eth3"
-#define PORT "5000"
-#define BUFLEN 256
+#define SPORT "5000"
+#define CPORT "5001"
+#define BUFLEN 1024
 #define BACKLOG 5
+#define CLIADDR "2002::a00:27ff:fea9:d6a1"
+
+
+int client(const char *host, char *buf);
+
 
 char *ip6_ntoa(struct in6_addr ip6){
 	static char str [INET6_ADDRSTRLEN];
 	inet_ntop(AF_INET6, &ip6, str, INET6_ADDRSTRLEN);
 	return str;
+}
+
+struct in6_addr ip6_aton(char *str){
+	struct in6_addr ip6;
+	inet_pton(AF_INET6, str, &ip6);
+	return ip6;
+}
+
+void subchar(char *t, const char *s){
+	int len;
+	int max = strlen(s);
+	for(len=0; len<max-1; len++){
+		*t++ = *s++;
+	}
+	*t = '\0';
 }
 
 void getifipv6addr(struct in6_addr *ip6, const char *device){
@@ -88,6 +109,40 @@ int tcplisten(const char *service){
 	return sock;
 }
 
+int tcpconnect(const char *host, const char *service){
+	int err;	
+	struct addrinfo hints;
+	struct addrinfo *res = NULL;
+	struct addrinfo *ai;
+	int sock;
+	
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET6;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_NUMERICSERV;
+
+	err = getaddrinfo(NULL, service, &hints, &res);
+	if(err != 0){
+		printf("getaddrinfo(): %s\n", gai_strerror(err));
+		return -1;
+	}
+
+	ai = res;
+	sock = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
+	if(sock < 0){
+		return -1;
+	}
+	int cs;
+	cs = connect(sock, ai->ai_addr, ai->ai_addrlen);
+	if(cs < 0){
+		freeaddrinfo(res);
+		return -1;
+	}
+	printf("con - NAT.\n");
+	freeaddrinfo(res);
+	return sock;
+}
+
 int read_line(int sock, char *p){
 	int len = 0;
 	while(1){
@@ -111,9 +166,9 @@ int read_line(int sock, char *p){
 	return len;
 }
 
-void server(char *ip6){
+void server(const char *ip){
 	int sock;
-	sock = tcplisten(PORT);
+	sock = tcplisten(SPORT);
 	if(sock < 0){
 		perror("server");
 		exit(1);
@@ -129,27 +184,57 @@ void server(char *ip6){
 			perror("accept");
 			exit(1);
 		}
-		printf("accept.\n");
+		printf("serv - accept.\n");
 		
 		int read_size;
-		char buf[BUFLEN];
-		read_size = read_line(cs, buf);
+		char readbuf[BUFLEN];
+		read_size = read_line(cs, readbuf);
 		if(read_size == 0)break;
-		printf("mes: %s", buf);
+		printf("serv - read: %s\n", readbuf);
+
 		char writebuf[BUFLEN];
-		sprintf(writebuf, "%s\n", ip6);
+		int clires;	
+		clires = client(ip, writebuf);
+		if(clires < 0){
+			sprintf(writebuf, "::1\n");
+		}
 		write(cs, writebuf, strlen(writebuf));
-		
-		printf("reject.\n");
+		printf("serv - write: %s\n", writebuf);
+
+		printf("serv - reject.\n");
 		//close(cs);
 	}
 }
 
+int client(const char *host, char *buf){
+	int sock;
+	sock = tcpconnect(host, CPORT);
+	if(sock < 0){
+		close(sock);
+		return -1;
+	}
+
+	char conbuf[] = "pull\n";
+	write(sock, conbuf, strlen(conbuf));	
+	int read_size;
+	char rebuf[BUFLEN];
+	read_size = read_line(sock, rebuf);
+	if(read_size != 0){
+		printf("mes: %s\n", rebuf);
+	}
+	subchar(buf, rebuf);
+	printf("discon - NAT.\n");
+	close(sock);
+	return 0;
+}
+
 int main(){
+	/*
 	struct in6_addr ip6;
 	getifipv6addr(&ip6, IF_NUM);
 	printf("%s - IPv6 address is %s.\n", IF_NUM, ip6_ntoa(ip6));
-
-	server(ip6_ntoa(ip6));
+	*/
+	char ip[] = CLIADDR;
+	server(ip);
 	return 0;
 }
